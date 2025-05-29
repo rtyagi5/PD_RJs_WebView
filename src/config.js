b643dd20a39d30047e6433045e20bcd5c0f26cde
pksingh0797 <pksingh0797@gmail.com>
2025-05-30 02:28:22 +0530

    reverted

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ const baseURL = "rehabranger.ai";
 
 export const getServiceUrl = (activityData) => {
   return {
-    // EXERCISE_SERVICE: `https://${activityData?.tenant}.${baseURL}/exercise-service`,
-    // USER_SERVICE: `https://${activityData?.tenant}.${baseURL}/user-service`,
-    EXERCISE_SERVICE: `http://34.233.242.35/exercise-service`,
-    USER_SERVICE: `http://34.233.242.35/user-service`,
+    EXERCISE_SERVICE: `https://${activityData?.tenant}.${baseURL}/exercise-service`,
+    USER_SERVICE: `https://${activityData?.tenant}.${baseURL}/user-service`,
+    // EXERCISE_SERVICE: `http://34.233.242.35/exercise-service`,
+    // USER_SERVICE: `http://34.233.242.35/user-service`,
   };
 };
